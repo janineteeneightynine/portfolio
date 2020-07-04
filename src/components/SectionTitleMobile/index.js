cf3f850fdd9b8d85e6122a0278e2a6cfbd1ef842
janineteeneightynine <janine.sleem@gmail.com>
2020-07-04 02:08:26 -0400

    blinking arrow

--- src/components/SectionTitleMobile/index.js
+++ src/components/SectionTitleMobile/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './SectionTitleMobile.css';
 
 function SectionTitleMobile(props) {
+
   return (
     <div className='sectionTitle'>
       <h1> {props.section} </h1>
+      <i className='fa fa-angle-right' id='sectionArrow'  
+        style={{
+          zIndex: 1, 
+            textAlign:'center', 
+            // backgroundColor: 'black', 
+            position: 'absolute', 
+            top: '45%',
+            left: '80%',
+            fontSize: '2.5rem',
+            width: '1%',
+            minWidth: '20px', 
+            borderRadius: '25%', 
+            color: 'gray'}}></i>
     </div>
   )
 }
